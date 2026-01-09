bccf7901fcec7768f240b66dd50ccac5d5156da5
Matthew Chipkin <mchipkin21@gmail.com>
2026-01-08 21:31:58 -0600

    fixed allow_origins to be * because of integration issues

--- rag_api.py
+++ rag_api.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Configure CORS (so your frontend can connect)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
